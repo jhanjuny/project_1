7cc2e8f9f5bc2bdd04daf33608521b1909e4ab0c
jhanjuny <itm2217@naver.com>
2025-07-30 10:44:35 +0900

    Auto commit 2025-07-30 10:44:35.23

---
+++ my-project/IGOR_1.0.2.py
@@ -0,0 +1,24 @@
+import erlab
+import erlab.analysis as era
+import erlab.plotting as eplt
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+import erlab.interactive as eri
+
+data1 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk1.ibw")
+#data2 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk2.ibw")
+#data3 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk3.ibw")
+#data4 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk4.ibw")
+
+print(data1)
+
+xr.set_options(display_expand_data=False)
+
+data1.sel(Energy =0.0, method="nearest").plot()
+
+eri.itool(data1)
+data1.qshow()
+
+
+

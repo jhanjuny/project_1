81277b1953dec5c3e7f0bf2572548e095ade7167
jhanjuny <itm2217@naver.com>
2025-08-04 13:45:55 +0900

    Auto commit 2025-08-04 13:45:55.33

--- my-project/IGOR_1.0.2.py
+++ my-project/IGOR_1.0.2.py
@@ -1,24 +1,17 @@
 import erlab
 import erlab.analysis as era
+import erlab.interactive as eri
 import erlab.plotting as eplt
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
-import erlab.interactive as eri
+import os
 
-data1 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk1.ibw")
-#data2 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk2.ibw")
-#data3 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk3.ibw")
-#data4 = xr.open_dataarray(r"D:/URP/3D modeling/chunkCube_3DEk4.ibw")
-
-print(data1)
-
+xr.set_options(keep_attrs=True)
 xr.set_options(display_expand_data=False)
 
-data1.sel(Energy =0.0, method="nearest").plot()
+#name= input('file name [ibw]=')
+data = xr.open_dataarray(r"D:/URP/3D modeling/0046.ibw")
 
-eri.itool(data1)
-data1.qshow()
-
-
-
+eri.itool(data)
+data.qshow()
